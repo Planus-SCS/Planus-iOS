572ad539a77672c541201baa26049e55735a1e92
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-20 23:06:35 +0900

    [feat] SignInViewModel 생성

---
+++ Planus/Presentation/SignInScene/ViewModel/SignInViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  SignInViewModel.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/03/20.
+//
+
+import Foundation
+import RxSwift
+
+class SignInViewModel {
+    var bag = DisposeBag()
+    
+    struct Input {
+        var kakaoSignInTapped: Observable<Void>
+        var googleSignInTapped: Observable<Void>
+        var appleSignInTapped: Observable<Void>
+    }
+    
+    struct Output {
+        var showKakaoSignInPage: Observable<Void>
+        var showGoogleSignInPage: Observable<Void>
+        var showAppleSignInPage: Observable<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let showKakaoSignInPage = PublishSubject<Void>()
+        let showGoogleSignInPage = PublishSubject<Void>()
+        let showAppleSignInPage = PublishSubject<Void>()
+        
+        input
+            .kakaoSignInTapped
+            .subscribe(onNext: {
+                showKakaoSignInPage.onNext(())
+            })
+            .disposed(by: bag)
+        
+        input
+            .googleSignInTapped
+            .subscribe(onNext: {
+                showGoogleSignInPage.onNext(())
+            })
+            .disposed(by: bag)
+        
+        input
+            .appleSignInTapped
+            .subscribe(onNext: {
+                showAppleSignInPage.onNext(())
+            })
+            .disposed(by: bag)
+        
+        return Output(
+            showKakaoSignInPage: showKakaoSignInPage.asObservable(),
+            showGoogleSignInPage: showGoogleSignInPage.asObservable(),
+            showAppleSignInPage: showAppleSignInPage.asObservable()
+        )
+    }
+}
