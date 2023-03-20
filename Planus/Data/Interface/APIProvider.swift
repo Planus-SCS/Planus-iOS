8a714333c5fb9a3e97610348f018502082543c44
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-21 02:04:46 +0900

    [feat] APIProvider 프로토콜 생성, NetworkManager 구현

---
+++ Planus/Data/Interface/APIProvider.swift
@@ -0,0 +1,14 @@
+//
+//  APIProvider.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/03/21.
+//
+
+import Foundation
+import RxSwift
+
+protocol APIProvider {
+//    func requestData(endPoint: APIEndPoint) -> Single<Data>
+//    func requestCodable<T: Codable>(endPoint: APIEndPoint, type: T.Type) -> Single<T>
+}
---
+++ Planus/Infrastructure/API/NetworkError.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkError.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/03/21.
+//
+
+import Foundation
+
+public enum NetworkError: Error, Equatable {
+    case httpRequestError
+    case httpResponseError
+    case unKnownError(String)
+    case unMatchingCodableTypeError
+    case invalidURLFormatError
+    case httpBodyEncodingError
+    case nilDataError
+}
