8e2df0158eb2f3c59aee4fd1484eff9407c9d72b
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-25 06:18:05 +0900

    [fix] todoFetch 인덱스 오류 해결

--- Planus/Presentation/HomeCalendarScene/ViewModel/HomeCalendarViewModel.swift
+++ Planus/Presentation/HomeCalendarScene/ViewModel/HomeCalendarViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewModel.swift
+//  HomeCalendarViewModel.swift
 //  Planus
 //
 //  Created by Sangmin Lee on 2023/03/23.
@@ -21,8 +21,8 @@ class HomeCalendarViewModel {
     let calendar = Calendar.current
     
     // for todoList caching
-    let cachingIndexDiff = 3
-    let cachingAmount = 5
+    let cachingIndexDiff = 8
+    let cachingAmount = 10
     
     let endOfFirstIndex = -100
     let endOfLastIndex = 1000
@@ -125,7 +125,7 @@ class HomeCalendarViewModel {
     
     func initTodoList(date: Date) {
         let fromIndex = currentIndex - cachingAmount
-        let toIndex = currentIndex + cachingAmount
+        let toIndex = currentIndex + cachingAmount + 1
         
         fetchTodoList(from: fromIndex, to: toIndex)
     }
@@ -155,38 +155,40 @@ class HomeCalendarViewModel {
         }
     }
     
-    // 미리 앞부분을 캐시를 로드해버리자
+    // 여기만 하면 이제 당분간은 투두 받아오는 부분 걱정도 없을듯? 근데 애니메이션을 어케 적용해야되냐?????
     func checkCacheLoadNeed() {
         guard let currentDate = try? self.currentDate.value() else { return }
         if latestPrevCacheRequestedIndex - currentIndex == cachingIndexDiff {
-            latestPrevCacheRequestedIndex = currentIndex
-
-            let fromIndex = currentIndex - cachingAmount - (cachingAmount - cachingIndexDiff)
-            let toIndex = currentIndex - (cachingAmount - cachingIndexDiff)
+            latestPrevCacheRequestedIndex = currentIndex //90 - 110
+            // 100에서 시작해서 92에 도달함. 리로드하고 어디부터? 83-90
+            let fromIndex = currentIndex - cachingAmount // 92 - 10 - (10-8)
+            let toIndex = currentIndex - (cachingAmount - cachingIndexDiff) //92 - (10-8) : 90
+            print("from: \(fromIndex), to: \(toIndex), current: \(currentIndex)")
             fetchTodoList(from: fromIndex, to: toIndex)
             
+            // 100에서 시작함 108에 도달함. 리로드 실시하고 어디부터 어디까지? 111 - 118 까지
+            // 108에서 리로드를 했음. 현재는 119까지 있음. 그럼 이제 또 116에서 리로드가 이뤄지겠지?
         } else if currentIndex - latestFollowingCacheRequestedIndex == cachingIndexDiff {
             latestFollowingCacheRequestedIndex = currentIndex
-            let fromIndex = currentIndex + cachingIndexDiff
-            let toIndex = currentIndex + cachingIndexDiff + cachingAmount
-            
+            let fromIndex = currentIndex + cachingAmount - cachingIndexDiff + 1 // 108 + 10 - 8 + 1
+            let toIndex = currentIndex + cachingAmount + 1 // 108 + 10
+            print("current: \(currentIndex), from: \(fromIndex), to: \(toIndex)")
             fetchTodoList(from: fromIndex, to: toIndex)
         }
     }
     
     func fetchTodoList(from fromIndex: Int, to toIndex: Int) {
+        
         guard let currentDate = try? self.currentDate.value() else { return }
         let fromMonth = calendar.date(byAdding: DateComponents(month: fromIndex - currentIndex), to: currentDate) ?? Date()
         let toMonth = calendar.date(byAdding: DateComponents(month: toIndex - currentIndex), to: currentDate) ?? Date()
         
-        let fromMonthStart = calendar.startOfDay(for: fromMonth)
-        let toMonthStart = calendar.startOfDay(for: toMonth)
+        let fromMonthStart = calendar.date(byAdding: DateComponents(day: -7), to: calendar.startOfDay(for: fromMonth)) ?? Date()
+        let toMonthStart = calendar.date(byAdding: DateComponents(day: 7), to: calendar.startOfDay(for: toMonth)) ?? Date()
 
         fetchTodoListUseCase.execute(from: fromMonthStart, to: toMonthStart)
             .subscribe(onSuccess: { [weak self] todoDict in
                 guard let self else { return }
-//                print(todoDict.keys)
-//                print("--------------------------------")
                 (fromIndex..<toIndex).forEach { index in
                     self.mainDayList[index] = self.mainDayList[index].map {
                         var dayViewModel = $0
