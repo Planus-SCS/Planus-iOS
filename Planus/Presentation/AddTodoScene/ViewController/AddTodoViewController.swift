ffd543704563ac83b757e5c5fc15182483d1199e
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-29 11:25:02 +0900

    [feat] CategorySelectView 분리

--- Planus/Presentation/AddTodoScene/ViewController/AddTodoViewController.swift
+++ Planus/Presentation/AddTodoScene/ViewController/AddTodoViewController.swift
@@ -38,7 +38,7 @@ class VC: UIViewController {
     }
 }
 
-class CategoryCell: UITableViewCell {
+class CategorySelectCell: UITableViewCell {
     static let reuseIdentifier = "category-cell"
     
     lazy var nameLabel: UILabel = {
@@ -90,105 +90,6 @@ class CategoryCell: UITableViewCell {
         nameLabel.text = name
         colorView.backgroundColor = color
     }
-}
-
-class CategorySelectView: UIView {
-    
-    var addNewItemButton: UIButton = {
-        let button = UIButton(frame: .zero)
-        button.setImage(UIImage(named: "addBtn"), for: .normal)
-        button.setTitle("새 카테고리 추가", for: .normal)
-        button.titleLabel?.font = UIFont(name: "Pretendard-Medium", size: 16)
-        button.imageEdgeInsets = .init(top: 0, left: -5, bottom: 0, right: 5)
-        button.tintColor = .black
-        button.setTitleColor(.black, for: .normal)
-        button.contentHorizontalAlignment = .leading
-        return button
-    }()
-    
-    var headerBarView: UIView = {
-        let view = UIView(frame: .zero)
-        return view
-    }()
-    
-    var backButton: UIButton = {
-        let image = UIImage(named: "pickerLeft") ?? UIImage()
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
-        button.setImage(UIImage(named: "pickerLeft"), for: .normal)
-        return button
-    }()
-    
-    var titleLabel: UILabel = {
-        let label = UILabel(frame: .zero)
-        label.text = "카테고리 선택"
-        label.font = UIFont(name: "Pretendard-Light", size: 16)
-        label.sizeToFit()
-        return label
-    }()
-    
-    var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero)
-        tableView.backgroundColor = UIColor(hex: 0xF5F5FB)
-        tableView.register(CategoryCell.self, forCellReuseIdentifier: CategoryCell.reuseIdentifier)
-        tableView.separatorInset.left = 16
-        tableView.separatorInset.right = 16
-        return tableView
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-        configureView()
-        configureLayout()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func configureView() {
-        self.backgroundColor = UIColor(hex: 0xF5F5FB)
-
-        self.layer.cornerRadius = 10
-        self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        self.clipsToBounds = true
-        
-        self.addSubview(headerBarView)
-        headerBarView.addSubview(backButton)
-        headerBarView.addSubview(titleLabel)
-        self.addSubview(addNewItemButton)
-        self.addSubview(tableView)
-    }
-    
-    func configureLayout() {
-        headerBarView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.height.equalTo(84)
-        }
-        backButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(20)
-            $0.centerY.equalToSuperview()
-        }
-        
-        titleLabel.snp.makeConstraints {
-            $0.center.equalToSuperview()
-        }
-        
-        addNewItemButton.snp.remakeConstraints {
-            $0.top.equalTo(self.headerBarView.snp.bottom)
-            $0.leading.equalToSuperview().inset(20)
-            $0.width.equalTo(140)
-            $0.height.equalTo(40)
-        }
-        
-        tableView.snp.makeConstraints {
-            $0.top.equalTo(addNewItemButton.snp.bottom)
-            $0.leading.trailing.bottom.equalToSuperview()
-        }
-    }
-    
-    var selectMode = false
-    
 }
 
 class CategoryCreateViewCell: UICollectionViewCell {
@@ -742,7 +643,7 @@ extension AddTodoViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: CategoryCell.reuseIdentifier, for: indexPath) as? CategoryCell,
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CategorySelectCell.reuseIdentifier, for: indexPath) as? CategorySelectCell,
               let viewModel else { return UITableViewCell() }
         
         cell.nameLabel.text = viewModel.categorys[indexPath.row].title
