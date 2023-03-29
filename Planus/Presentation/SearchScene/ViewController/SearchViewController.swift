7297e746b5b73062e4a1fc2c34de7df2df4b994a
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-30 05:55:53 +0900

    [chore] SearchViewController, SearchViewModel 생성

---
+++ Planus/Presentation/SearchScene/ViewController/SearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewController.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/03/30.
+//
+
+import Foundation
---
+++ Planus/Presentation/SearchScene/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/03/30.
+//
+
+import Foundation
