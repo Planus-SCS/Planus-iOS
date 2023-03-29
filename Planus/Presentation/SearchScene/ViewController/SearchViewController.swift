653245ba233c06523e4b7a79f65eb8b7a5cf95c3
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-03-30 06:14:48 +0900

    [feat] SearchViewController collectionView 레이아웃 생성 메서드 구현

--- Planus/Presentation/SearchScene/ViewController/SearchViewController.swift
+++ Planus/Presentation/SearchScene/ViewController/SearchViewController.swift
@@ -5,4 +5,61 @@
 //  Created by Sangmin Lee on 2023/03/30.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    // 필요한거 화면에 뿌려줄 컬렉션 뷰, 근데 검색 결과를 보여줄 땐 한 뎁스를 타고 들어가야 한다!
+    
+    lazy var resultCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createSection())
+        collectionView.register(<#T##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+        return collectionView
+    }()
+    
+    var headerView: UIView = {
+        let view = UIView(frame: .zero)
+        return view
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func configureView() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+}
+
+extension SearchViewController {
+    private func createSection() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .absolute(250))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 2)
+        group.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+
+        return layout
+    }
+}
