7a27efb40379a32de4f327ac2c6d68bbd380db66
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-04-04 12:41:43 +0900

    [feat] JoinedGroupDetailViewController 생성

---
+++ Planus/Presentation/JoinedGroupDetailScene/ViewController/JoinedGroupDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JoinedGroupDetailViewController.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/04/04.
+//
+
+import UIKit
+import RxSwift
+
+class JoinedGroupDetailViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
