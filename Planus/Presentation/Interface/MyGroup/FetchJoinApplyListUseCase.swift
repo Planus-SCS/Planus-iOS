1ddd0f79c3e80f4a388ed4fe53ca89cbc4e7c430
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-05-09 23:10:15 +0900

    [feat] FetchJoinApplyListUseCase 프로토콜 선언 및 구현

---
+++ Planus/Presentation/Interface/MyGroup/FetchJoinApplyListUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  FetchJoinApplyListUseCase.swift
+//  Planus
+//
+//  Created by Sangmin Lee on 2023/05/09.
+//
+
+import Foundation
+import RxSwift
+
+protocol FetchJoinApplyListUseCase {
+    func execute(token: Token) -> Single<[GroupJoinApply]>
+}
