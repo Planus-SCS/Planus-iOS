ee6079aa808026a84749234e6bef207d28bcd069
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-05-15 16:23:47 +0900

    [fix] SpringableButton의 spring 방식을 addTarget을 통해서 변경하던 것을 isHighlighted 프로퍼티의 변화를 감지해 적용하도록 변경

--- Planus/Presentation/Common/View/SpringableButton.swift
+++ Planus/Presentation/Common/View/SpringableButton.swift
@@ -8,29 +8,33 @@
 import UIKit
 
 class SpringableButton: UIButton {
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                springIn()
+            } else {
+                springOut()
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        configureAction()
+        self.adjustsImageWhenHighlighted = false
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configureAction() {
-        self.addTarget(self, action: #selector(springIn), for: .touchDown)
-        self.addTarget(self, action: #selector(springOut), for: .touchUpInside)
-        self.addTarget(self, action: #selector(springOut), for: .touchUpOutside)
-    }
-    
-    @objc func springIn() {
+    func springIn() {
         UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseInOut, animations: {
             self.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
         })
     }
     
-    @objc func springOut() {
+    func springOut() {
         UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut, animations: {
             self.transform = CGAffineTransform(scaleX: 1, y: 1)
         })
