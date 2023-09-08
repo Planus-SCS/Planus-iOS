bf9a744c6d8571a305effd88a7fed51be0923021
Sangmin627 <109147915+Sangmin627@users.noreply.github.com>
2023-09-09 00:24:06 +0900

    [fix] handleRetry 메서드에서 noTokenError의 경우에도 기존 로직이 두번 실행되던 문제를 해결

--- Planus/Util/RxSwift/PrimitiveSequence+Ext.swift
+++ Planus/Util/RxSwift/PrimitiveSequence+Ext.swift
@@ -9,34 +9,20 @@ import Foundation
 import RxSwift
 
 extension PrimitiveSequence where Trait == SingleTrait { //어떤상황이면 어떤 옵저버블을 끼워넣고 다시 재시도 하도록
-
+    
     func handleRetry<T>(retryObservable: Single<T>, errorType: NetworkManagerError) -> Single<Element> {
+        
         return retry(when: { errorObservable in
-            errorObservable.flatMap { error -> Single<Element> in
+            errorObservable.flatMap { error -> Single<T> in
                 guard let error = error as? NetworkManagerError,
                       error == errorType else {
-                    return Single<Element>.create { emmiter -> Disposable in
-                        emmiter(.failure(error))
-                        return Disposables.create()
-                    }
+                    return Single<T>.error(error) // 에러가 아닌 경우 에러를 방출, 이땐 왜 리트라이 안하고? 이해가 안가네
                 }
-                return retryObservable.flatMap { retrial in
-                    return self
-                }
-            }
-        })
-    }
-    
-    
-}
-
-extension Observable {
-    func handleRetry<T, E: Error>(retryObservable: Observable<T>, errorType: E) -> Observable<Element> {
-        return retry(when: { (error: Observable<E>) in
-            error.flatMap { error -> Observable<T> in
                 return retryObservable
             }
         })
-        .retry(2)
     }
+    
+    
 }
+
